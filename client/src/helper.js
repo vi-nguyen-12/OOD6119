2e673a1f15cc64491d53877ae9091bce35c21678
Chloe <chloe.ng.1292@gmail.com>
2023-11-26 23:20:49 -0600

    format date

---
+++ client/src/helper.js
@@ -0,0 +1,15 @@
+export const get_due_date = (borrow_date_str) => {
+  const date = new Date(borrow_date_str);
+  date.setDate(date.getDate() + 15);
+  const year = date.getFullYear();
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+  return `${year}-${month}-${day}`;
+};
+export const get_today_str = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = ("0" + (date.getMonth() + 1)).slice(-2);
+  const day = ("0" + date.getDate()).slice(-2);
+  return `${year}-${month}-${day}`;
+};
