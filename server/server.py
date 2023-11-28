6c72bc285d053705ffcd4c6013ae32bfc50336a2
NyeishaJPace <nyeisha.j.pace@gmail.com>
2023-11-28 08:36:24 +0000

    added database code to server file

--- server/server.py
+++ server/server.py
@@ -5,11 +5,20 @@ from flask import Flask
 from flask_cors import CORS
 from api.bookApi import book_api
 from api.visitorApi import visitor_api
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 # [Nyeisha] Should connect to database
+db = mysql.connector.connect(
+    host='host',
+    user='user',
+    password='password',
+    database='database'
+)
+
+cursor = db.cursor()
 
 # API routes
 app.register_blueprint(book_api, url_prefix='/api/books')
