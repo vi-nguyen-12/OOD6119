61b0b1f4fa2d0635c35cda501c12bc6d21f3050d
Chloe <chloe.ng.1292@gmail.com>
2023-11-30 18:21:36 -0600

    add catalog

--- client/src/pages/admin/Catalog.js
+++ client/src/pages/admin/Catalog.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Table from "react-bootstrap/Table";
 
 const Catalog = () => {
-  return <div>Catalog</div>;
+  const [logs, setLogs] = useState([]);
+  useEffect(() => {
+    const get_logs = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:5000/api/admins/logger`
+        );
+
+        setLogs(response.data);
+      } catch (err) {
+        alert(err.message);
+      }
+    };
+    get_logs();
+  }, []);
+  return (
+    <div>
+      <h4>Admin Logger</h4>
+      <Table striped className="mt-5">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Activity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {logs.map((log, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{log}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default Catalog;
